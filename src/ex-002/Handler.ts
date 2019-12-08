cf3930d5858840164829ae8ad06791abdfe1c269
aerocode <meteo_meteo@naver.com>
2019-12-08 09:41:27 +0900

    Add ex-002; Lambda # Query String

--- src/ex-002/Handler.ts
+++ src/ex-002/Handler.ts
@@ -1,25 +1,30 @@
 import { APIGatewayEvent } from "aws-lambda";
 
-// Hello, World!를 출력한다.
-// GET 형식의 쿼리 문자열이 있다면,
-// 쿼리 문자열의 이름과 값을 한 줄씩 차례대로 적는다.
-// 쿼리 문자열이 없다면 안내 메시지를 함께 출력한다.
+/**
+ * 맨 처음 Hello, World!를 출력하고.
+ * 그 다음 줄에는 HTTP Method(GET | POST)를 출력한다.
+ *
+ * 그 다음 줄 부터는 데이터의 이름과 값을 한 줄씩 출력하고,
+ * 데이터가 없다면 "함께 보내진 데이터가 없습니다."를 출력한다.
+ */
 export const echo = async (event: APIGatewayEvent) => {
     let paramMap = event.queryStringParameters;
-    let message = ["Hello, World!"];
+    let messages = [];
+    messages.push("Hello, World!");
+    messages.push(event.httpMethod);
 
     if (paramMap != null) {
         Object.keys(paramMap)
             .map(key => `${key} : ${paramMap!![key]}`)
             .forEach(str => {
-                message.push(str);
+                messages.push(str);
             });
     } else {
-        message.push("입력된 쿼리 문자열이 없습니다.");
+        messages.push("함께 보내진 데이터가 없습니다.");
     }
 
     return {
         statusCode: 200,
-        body: message.join("\n")
+        body: messages.join("\n")
     };
 };

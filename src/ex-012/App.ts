c665c28db72528aff6b1d648b40ff9c00b51d99c
aerocode <meteo_meteo@naver.com>
2019-12-08 23:29:15 +0900

    Add ex-012; Serverless Express # Session Cookie

---
+++ src/ex-012/App.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(cookieParser());
+
+/**
+ * Exercise 12)
+ *      - AWS Serverless Express
+ *      - Session Cookie Counter (With cookie-parser)
+ */
+const rootRouter = express.Router();
+rootRouter.get("/", async function(req, res) {
+    const count: number =
+        !req.cookies.count || isNaN(Number(req.cookies.count))
+            ? 0
+            : Number(req.cookies.count);
+    res.cookie("count", count + 1);
+    res.send(count.toString());
+});
+app.use("/", rootRouter);
+
+export default app;

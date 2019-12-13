c956789146002f6aa43ed79d140387be4a5ec903
aerocode <meteo_meteo@naver.com>
2019-12-13 23:50:50 +0900

    Add ex-019; Serverless GraphQL # GraphQL Union Object

---
+++ src/ex-019/App.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import GraphqlHTTP from "express-graphql";
+import schema from "./Schema";
+
+const app = express();
+
+/**
+ * Exercise 19)
+ *  유니온 타입 사용하기.
+ *
+ */
+app.use(
+    "/",
+    GraphqlHTTP({
+        schema: schema,
+        graphiql: true
+    })
+);
+export default app;
---
+++ src/ex-019/Schema.ts
@@ -0,0 +1,80 @@
+import {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLUnionType
+} from "graphql";
+
+const TruthyNumberType = new GraphQLObjectType({
+    name: "TruthyNumberType",
+    fields: {
+        number: { type: GraphQLInt },
+        result: {
+            type: GraphQLString,
+            resolve() {
+                return "true";
+            }
+        },
+        x: {
+            type: GraphQLString,
+            resolve() {
+                return "truthy object field";
+            }
+        }
+    },
+    isTypeOf: data => data.number
+});
+
+const FalsyNumberType = new GraphQLObjectType({
+    name: "FalsyNumberType",
+    fields: {
+        number: { type: GraphQLInt },
+        result: {
+            type: GraphQLString,
+            resolve() {
+                return "false";
+            }
+        },
+        y: {
+            type: GraphQLString,
+            resolve() {
+                return "falsy object field";
+            }
+        }
+    },
+    isTypeOf: data => !data.number
+});
+
+const BinaryNumberUnion = new GraphQLUnionType({
+    name: "BinaryNumberUnion",
+    types: [TruthyNumberType, FalsyNumberType],
+    resolveType: SearchableTypeResolver
+});
+
+function SearchableTypeResolver(data: any): GraphQLObjectType {
+    return data.number ? TruthyNumberType : FalsyNumberType;
+}
+
+let query = new GraphQLObjectType({
+    name: "test",
+    fields: {
+        test: {
+            type: BinaryNumberUnion,
+            args: {
+                number: {
+                    type: GraphQLInt
+                }
+            },
+            resolve: (source, args) => {
+                return args;
+            }
+        }
+    }
+});
+
+let schema = new GraphQLSchema({
+    query: query
+});
+
+export default schema;

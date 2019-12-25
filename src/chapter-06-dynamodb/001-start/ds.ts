2606fdb9c04301d84781c06945dc00c7dcc11935
AeroCode <meteo_meteo@naver.com>
2019-12-25 19:46:51 +0900

    add dynamodb start

---
+++ src/chapter-06-dynamodb/001-start/ds.ts
@@ -0,0 +1,13 @@
+import { hashKey, rangeKey, table } from "@aws/dynamodb-data-mapper-annotations";
+
+// table 데코레이터에 테이블명을 적고,
+// 해시키와 범위키에 각각 hasKey, rangeKey 데코레이터를 붙인다.
+//
+@table("cat")
+export class Cat {
+    @hashKey()
+    name!: string;
+
+    @rangeKey({ defaultProvider: () => new Date() })
+    createdAt!: Date;
+}

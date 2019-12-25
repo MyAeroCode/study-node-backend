262961f9538b39687fcd13b8b3feda58c1dbc8bb
AeroCode <meteo_meteo@naver.com>
2019-12-25 21:12:35 +0900

    add batch get/put

--- src/chapter-06-dynamodb/002-batch/cat-samples.ts
+++ src/chapter-06-dynamodb/002-batch/cat-samples.ts
@@ -2,7 +2,7 @@ import { Cat } from "./cat-type";
 
 export let samples = new Map<string, Cat>();
 let now = Date.now();
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 1000; i++) {
     let name = `cat_${now}_${i}`;
     samples.set(
         name,
